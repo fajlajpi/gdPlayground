5b94fabaeaa1134070d4cf6454a734a3d841bd3e
fajlajpi <fajlajpi@gmail.com>
2024-02-27 22:56:00 +0100

    minor change

--- GridMovement/scripts/audio_stream_player.gd
+++ GridMovement/scripts/audio_stream_player.gd
@@ -4,7 +4,8 @@ extends AudioStreamPlayer
 @export var pitch_sine_wave_test : bool = false
 var pitch_min : float = 0.8
 var pitch_max : float = 1.2
-var pitch_change : float = 1.0
+var pitch_change_timer : float = 0.0
+var amplitude : float = 1.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,5 +17,5 @@ func _process(delta):
 	if not pitch_sine_wave_test:
 		pitch_scale = pitch_value_test
 	else:
-		pitch_change += sin(pitch_change + delta)
-		pitch_scale = pitch_change
+		pitch_change_timer += delta
+		pitch_scale = sin(pitch_change_timer) * amplitude
