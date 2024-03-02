239035a9b0e68aebcea283541fc49279d592b1df
fajlajpi <fajlajpi@gmail.com>
2024-03-02 20:57:51 +0100

    Node based FSM
    Begun writing a node-based Finite State Machine based on GDQuest tutorial

---
+++ GridMovement/scripts/state.gd
@@ -0,0 +1,28 @@
+class_name State
+extends Node
+
+var state_machine : StateMachine = null
+
+func handle_input(_event : InputEvent) -> void:
+	pass
+	
+func update(_delta : float) -> void:
+	pass
+
+func physics_update(_delta : float) -> void:
+	pass
+
+func enter(_msg := {}) -> void:
+	pass
+	
+func exit() -> void:
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
