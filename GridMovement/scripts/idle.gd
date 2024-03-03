f3aa7bfdc0f37525231b8f5610ab8d716d93bd6e
fajlajpi <fajlajpi@gmail.com>
2024-03-04 00:16:19 +0100

    Idle state
    Started fleshing out the Idle state for the FSM

--- GridMovement/scripts/idle.gd
+++ GridMovement/scripts/idle.gd
@@ -2,3 +2,18 @@ class_name Idle
 extends PlayerState
 
 # IDLE state waits for input to take appropriate actions
+
+func handle_input(event) -> void:
+	pass
+	
+
+func enter(msg:={}) -> void:
+	pass
+	
+	
+func exit() -> void:
+	pass
+	
+
+func update(delta: float) -> void:
+	pass
--- GridMovement/scripts/player_state.gd
+++ GridMovement/scripts/player_state.gd
@@ -3,6 +3,8 @@ extends State
 
 var player : Player
 
+enum Actions {MOVE, INTERACT, OTHER}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await owner.ready
